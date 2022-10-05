abd95fc31eb9ae8d64466b54ad14e9e12ed54868
KeithAverre <kmaverre@alaska.edu>
2022-10-05 12:42:15 -0800

    fix for create page

--- encyclopedia/forms.py
+++ encyclopedia/forms.py
@@ -3,16 +3,16 @@ class NewTaskForm(forms.Form):
     entryTitle = forms.CharField(label = "Title")
     entryContent = forms.CharField(label="content",widget = forms.Textarea())
 
-    def __init__(self, session =None, title= None, content = None):
-        self.session = session
-        self.entryTitle = title
-        self.entryContent = content
-    def setTitle(self, title):
-        self.entryTitle = title
+    #def __init__(self, session =None, title= None, content = None):
+    #    self.session = session
+    #    self.entryTitle = title
+    #    self.entryContent = content
+    #def setTitle(self, title):
+    #    self.entryTitle = title
 
-    def setContent(self, content):
-        self.entryContent = content
+    #def setContent(self, content):
+    #    self.entryContent = content
 
-    def setEntry(self, title, content):
-        self.entryTitle = title
-        self.entryContent = content
+    #def setEntry(self, title, content):
+    #    self.entryTitle = title
+    #    self.entryContent = content
