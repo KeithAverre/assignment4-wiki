cdfb05b53bb1a9b278a4a6ad73a2b8206321e9a3
Keith Matthew Averre Jr <kmaverre@alaska.edu>
2022-10-04 22:58:21 -0800

    Adding an entry viewing page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/create_page", views.create_page, name="create_page"),
+    path("wiki/<str:TITLE>", views.entry_page, name="TITLE"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 
 from . import util
-
+from markdown2 import Markdown
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+def entry_page(request, TITLE):
+    page = util.get_entry(TITLE)
+    if page is not None:
+        #markdown magic for the win
+        markdowner = Markdown()
+        content = markdowner.convert(page)
+        return render(request, "encyclopedia/entry_page.html", {"content": content})
+    else:
+        return render(request, "encyclopedia/index.html")
 
+def create_page(request):
+    return -1
